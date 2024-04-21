8c7521a2d2455c4ecf76de635e58199cafed892e
Armin-Muric <armin.muric@utb.ecutbildning.se>
2024-04-21 04:07:32 +0200

    Structure updated

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc5e1ba89c7019692265094a74605c07638f5756")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cd5511c749650230715f078446a7921ca1f16ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
