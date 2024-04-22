a616a5049d9a9f72319311344b866d2f37b7dcfd
Armin-Muric <armin.muric@utb.ecutbildning.se>
2024-04-22 05:33:13 +0200

    Structure updated

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e50dd0d0adc68ed24a19bcebed1a0146d8d6ddf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c7521a2d2455c4ecf76de635e58199cafed892e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
